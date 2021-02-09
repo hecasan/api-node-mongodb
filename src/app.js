5374ecabd9879e1c5a30ef09c689410a89ace0f4
Héte Caetano <hetecaetano@gmail.com>
2021-02-09 00:49:45 -0300

    Criado um CRUD REST

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
@@ -12,6 +16,24 @@ const route = router.get('/', (req, res, next) => {
   });
 });
 
+
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const update = router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  res.status(201).send({ id: id, item: req.body });
+});
+
+const remove = router.delete('/', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', update);
+app.use('/products', remove);
 
 module.exports = app;
